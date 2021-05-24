15c61d7bff019472e09c4113cc269b335ba1ca03
Method.Jiao <45934872+MethodJiao@users.noreply.github.com>
2021-05-24 17:43:49 +0800

    Update EncryptionString.cpp

--- EncryptionString.cpp
+++ EncryptionString.cpp
@@ -5,73 +5,16 @@
 #include <streambuf>  
 using namespace std;
 
-int key = 0x1AA4A;
-
-void encode(const char* rawData, char*& encodedData)
+int key[] = {3,5,1,6,7,2,4,9,8,'a','$','&',3,'#'}
+void encodeXOR(char* in char*out,size_t size)
 {
-	for (int i = 0; i < strlen(rawData); i++) {
-		encodedData[i] = rawData[i] + key;
+	for(size_t i = 0; i < size; i++)
+	{
+		out[i] = in[i] ^ key[i%14];
 	}
 }
 
-void decode(const char* encodeData, char*& decodedData)
+void decodeXOR(char* in,char* out,size_t size)
 {
-	for (int i = 0; i < strlen(encodeData); i++) {
-		decodedData[i] = encodeData[i] - key;
-	}
-}
-
-
-void produceEncodeFile(string source, string dest)
-{
-	std::ifstream sourceStream(source);
-	std::string sourceStr((std::istreambuf_iterator<char>(sourceStream)),
-		std::istreambuf_iterator<char>());
-
-	ofstream encodeFile;
-	encodeFile.open(dest, ios::out);
-	if (!encodeFile)
-		exit(1);
-
-	char* encodedData = (char*)malloc(sizeof(char) * strlen(sourceStr.c_str()) + 1);
-	if (encodedData == nullptr)
-		exit(1);
-
-	memset(encodedData, 0, sizeof(char) * strlen(sourceStr.c_str()) + 1);
-	encode(sourceStr.c_str(), encodedData);
-
-
-	encodeFile << encodedData;
-	encodeFile.close();
-	free(encodedData);
-}
-void formEncodeFileProduceDecodeFile(string source, string dest)
-{
-
-	std::ifstream sourceStream(source);
-	std::string encodeSourceStr((std::istreambuf_iterator<char>(sourceStream)),
-		std::istreambuf_iterator<char>());
-
-	ofstream decodeFile;
-	decodeFile.open(dest, ios::out);
-	if (!decodeFile)
-		exit(1);
-
-	char* decodedData = (char*)malloc(sizeof(char) * strlen(encodeSourceStr.c_str()) + 1);
-	if (decodedData == nullptr)
-		exit(1);
-
-	memset(decodedData, 0, sizeof(char) * strlen(encodeSourceStr.c_str()) + 1);
-	decode(encodeSourceStr.c_str(), decodedData);
-
-	decodeFile << decodedData;
-	decodeFile.close();
-	free(decodedData);
-
-}
-int main() 
-{
-	produceEncodeFile("C:\\Users\\Method.Jiao-WTG\\Desktop\\rsa\\Debug\\MepConfig.xml", "C:\\Users\\Method.Jiao-WTG\\Desktop\\rsa\\Debug\\MepConfig1.xml");
-
-	formEncodeFileProduceDecodeFile("C:\\Users\\Method.Jiao-WTG\\Desktop\\rsa\\Debug\\MepConfig1.xml", "C:\\Users\\Method.Jiao-WTG\\Desktop\\rsa\\Debug\\MepConfig2.xml");
+	encodeXOR(in,out,size);
 }
